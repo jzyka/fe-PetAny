063a5846354dbacc55bf995a8b597f2b254f6fa2
jzyka <auliazykha@gmail.com>
2023-01-17 10:07:40 +0700

    feat: [ZA] home unfinised

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>fkhbgfhbhb</h1>
+  <section class="home-page">
+    <v-container>
+      <v-row>
+        <v-carousel cycle height="400" hide-delimiter-background>
+          <v-carousel-item
+            v-for="(item, i) in items"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
