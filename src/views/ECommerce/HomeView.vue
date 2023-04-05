447de247ac502a51e342c1c79513cd54a47eafad
jzyka <auliazykha@gmail.com>
2023-04-05 09:12:51 +0700

    fix: [ZA] verification snackbar

--- src/views/ECommerce/HomeView.vue
+++ src/views/ECommerce/HomeView.vue
@@ -4,7 +4,13 @@
       <v-container>
         <v-row>
           <v-carousel cycle height="400" hide-delimiter-background>
-            <v-carousel-item v-for="(banner, i) in banners" :key="i" :src="banner.src" reverse-transition="fade-transition" transition="fade-transition"></v-carousel-item>
+            <v-carousel-item
+              v-for="(banner, i) in banners"
+              :key="i"
+              :src="banner.src"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+            ></v-carousel-item>
           </v-carousel>
         </v-row>
       </v-container>
@@ -15,18 +21,36 @@
         <v-row>
           <v-cols cols="12">
             <div class="slider--cards__title">
-              <p class="slider-title">Buat janji dengan dokter hewan terdekat</p>
+              <p class="slider-title">
+                Buat janji dengan dokter hewan terdekat
+              </p>
               <!-- <router-link to="/book-appoinment" class="see-more">Lihat Semua</router-link> -->
             </div>
-            <v-slide-group v-model="VetCard" selected-class="bg-primary" multiple show-arrows>
+            <v-slide-group
+              v-model="VetCard"
+              selected-class="bg-primary"
+              multiple
+              show-arrows
+            >
               <v-slide-item v-for="(vetAll, i) in vetAlls" :key="i">
-                <router-link tag="a" :to="vetAll.links.self" class="card--vetproduct ma-2">
+                <router-link
+                  tag="a"
+                  :to="vetAll.links.self"
+                  class="card--vetproduct ma-2"
+                >
                   <div class="card--vetproduct__image">
-                    <v-img :src="vetAll.petshop_image" cover :aspect-ratio="4 / 3" class="rounded-lg elevation-1"></v-img>
+                    <v-img
+                      :src="vetAll.petshop_image"
+                      cover
+                      :aspect-ratio="4 / 3"
+                      class="rounded-lg elevation-1"
+                    ></v-img>
                   </div>
                   <div class="card--vetproduct__detail">
                     <p class="name">{{ vetAll.petshop_name }}</p>
-                    <p class="hours-price">{{ vetAll.jam_buka }}-{{ vetAll.jam_tutup }}</p>
+                    <p class="hours-price">
+                      {{ vetAll.jam_buka }}-{{ vetAll.jam_tutup }}
+                    </p>
                     <p class="location">{{ vetAll.district }}</p>
                   </div>
                 </router-link>
@@ -42,27 +66,51 @@
         <v-row>
           <div>
             <div class="slider--cards__title">
-            <p class="slider-title">Beli Product</p>
-            <!-- <router-link to="/" class="see-more">Lihat Semua</router-link> -->
-          </div>
-          <v-slide-group v-model="VetCard" selected-class="bg-primary" multiple show-arrows>
-            <v-slide-item v-for="(productAll, i) in productAlls" :key="i">
-              <router-link tag="a" :to="productAll.link" class="card--vetproduct ma-2">
-                <div class="card--vetproduct__image">
-                  <v-img :src="productAll.data.image" cover :aspect-ratio="4 / 3" class="rounded-lg"></v-img>
-                </div>
-                <div class="card--vetproduct__detail">
-                  <p class="name">{{ productAll.data.name }}</p>
-                  <p class="hours-price">Rp {{ productAll.data.price }}</p>
-                  <p class="location">Tersisa {{ productAll.data.stock }}</p>
-                </div>
-              </router-link>
-            </v-slide-item>
-          </v-slide-group>
+              <p class="slider-title">Beli Product</p>
+              <!-- <router-link to="/" class="see-more">Lihat Semua</router-link> -->
+            </div>
+            <v-slide-group
+              v-model="VetCard"
+              selected-class="bg-primary"
+              multiple
+              show-arrows
+            >
+              <v-slide-item v-for="(productAll, i) in productAlls" :key="i">
+                <router-link
+                  tag="a"
+                  :to="productAll.link"
+                  class="card--vetproduct ma-2"
+                >
+                  <div class="card--vetproduct__image">
+                    <v-img
+                      :src="productAll.data.image"
+                      cover
+                      :aspect-ratio="4 / 3"
+                      class="rounded-lg"
+                    ></v-img>
+                  </div>
+                  <div class="card--vetproduct__detail">
+                    <p class="name">{{ productAll.data.name }}</p>
+                    <p class="hours-price">Rp {{ productAll.data.price }}</p>
+                    <p class="location">Tersisa {{ productAll.data.stock }}</p>
+                  </div>
+                </router-link>
+              </v-slide-item>
+            </v-slide-group>
           </div>
         </v-row>
       </v-container>
     </section>
+
+    <v-snackbar v-model="verifSnackbar" :timeout="timeout" top>
+      Cek Email anda dan verifikasi akun
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="verifSnackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -73,7 +121,9 @@ export default {
   data: () => ({
     VetCard: [],
     vetAlls: [],
+    verifSnackbar: false,
     productAlls: [],
+    timeout: 2000,
     banners: [
       {
         src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
@@ -90,12 +140,24 @@ export default {
     ],
   }),
 
-    async mounted() {
-      await this.getVetAll();
-      await this.getProductAll();
-    },
+  async mounted() {
+    await this.getVetAll();
+    await this.getProductAll();
+  },
 
-    methods: {
+  created() {
+    this.getLocalStorage();
+  },
+
+  methods: {
+    getLocalStorage() {
+      this.localStorage = JSON.parse(localStorage.getItem("data"));
+      console.log("ini local", localStorage.data.email_verified_at);
+
+      if (this.localStorage.data.email_verified_at === null) {
+        this.verifSnackbar = true;
+      }
+    },
     async getVetAll() {
       try {
         const res = await axios.get(`${this.$api}/get-petshop-with-schedule`);
@@ -111,10 +173,10 @@ export default {
     async getProductAll() {
       try {
         const res = await axios.get(`${this.$api}/get-product`);
-        this.productAlls = res.data.map(product => ({
-        data: product.data,
-        link: product.links,
-      }));
+        this.productAlls = res.data.map((product) => ({
+          data: product.data,
+          link: product.links,
+        }));
       } catch (error) {
         console.log(error);
       }
